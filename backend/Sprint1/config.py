b2044bb59196f67206e3fdb626f331e4dbc06806
farhansadaat <147191301+farhansadaat@users.noreply.github.com>
2026-03-29 16:57:02 -0400

    Add files via upload

---
+++ backend/Sprint1/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client
+
+# Load .env automatically
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+if not SUPABASE_URL or not SUPABASE_ANON_KEY:
+    raise Exception("Missing SUPABASE_URL or SUPABASE_ANON_KEY in .env")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
